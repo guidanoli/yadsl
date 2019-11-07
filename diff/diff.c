b108d0e662324ea6b0949292d7b867b58a3ac5c5
guidanoli <guidanoli@hotmail.com>
2019-11-06 23:03:50 -0300

    optimized diff
    * space complexity is now linear

--- diff/diff.c
+++ diff/diff.c
@@ -47,36 +47,37 @@ int diff(char *s1, char *s2)
     if (!s1 || !s2) return 1;
 
     int l1 = strlen(s1), l2 = strlen(s2);
-    int **M = malloc((l1+1)*sizeof(int*));
+    int *M = malloc((l1+1)*sizeof(int));
     if (!M) return 1;
-
-    for (int i = 0; i <= l1; i++) {
-        M[i] = malloc((l2+1)*sizeof(int));
-        if (!M[i]) return 1;
+    int *N = malloc((l1+1)*sizeof(int));
+    if (!N) {
+        free(M);
+        return 1;
     }
 
     for (int i = 0; i <= l1; i++)
-        M[i][0] = i*DELTA;
-
-    for (int j = 0; j <= l2; j++)
-        M[0][j] = j*DELTA;
+        M[i] = i*DELTA;
 
     int v[3];
-    for (int i = 1; i <= l1; i++) {
-        for (int j = 1; j <= l2; j++) {
-            v[0] = alpha(s1[i-1], s2[j-1]) + M[i-1][j-1];
-            v[1] = DELTA + M[i-1][j];
-            v[2] = DELTA + M[i][j-1];
+    for (int j = 1; j <= l2; j++) {
+        for (int i = 0; i <= l1; i++)
+            N[i] = M[i];
+        M[0] = j*DELTA;
+        for (int i = 1; i <= l1; i++) {
+            v[0] = alpha(s1[j-1], s2[i-1]) + N[i-1];
+            v[1] = DELTA + M[i-1];
+            v[2] = DELTA + N[i];
             int lv = v[0];
             for (int k = 1; k < 3; k++)
                 if (v[k] < lv)
                     lv = v[k];
-            M[i][j] = lv;
+            M[i] = lv;
         }
     }
 
-    printf("Cost = %d\n", M[l1-1][l2-1]);
+    printf("Cost = %d\n", M[l1]);
 
     free(M);
+    free(N);
     return 0;
 }
