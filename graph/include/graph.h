db6d3dcb65751e917baa77fdd0b91986cca1a5f2
Guilherme Dantas <guidanoli@hotmail.com>
2019-12-13 14:28:18 -0300

    Documentation
    * Added docstrings to graph and set header files

--- graph/include/graph.h
+++ graph/include/graph.h
@@ -3,12 +3,12 @@
 
 /**
 * A Graph starts with a fixed number of vertices and no edges.
-* You are able to add and remove edges and check neighbourhoods.
+* It is possible to add and remove edges and check neighbourhoods.
 */
 
 typedef enum
 {
-    /* All went ok */
+    /* Everything went as excepted */
     GRAPH_RETURN_OK,
 
     /* Invalid parameter was provided */
@@ -39,11 +39,88 @@ GraphReturnID;
 
 typedef struct Graph Graph;
 
+/**
+* Create a graph with no edges
+* ppGraph   address of pointer to graph
+* size      maximum number of vertices
+* Possible errors:
+* GRAPH_RETURN_INVALID_PARAMETER:
+*   - "ppGraph" is NULL
+*   - "size" is 0
+* GRAPH_RETURN_MEMORY
+* GRAPH_RETURN_UNKNOWN_ERROR
+*/
 GraphReturnID graphCreate(Graph **ppGraph, size_t size);
+
+/**
+* Get number of vertices in graph
+* pGraph    pointer to graph
+* pSize     address to variable that will hold the number of vertices
+* Possible errors:
+* GRAPH_RETURN_INVALID_PARAMETER:
+*   - "pGraph" is NULL
+*   - "pSize" is NULL
+*/
 GraphReturnID graphGetNumberOfVertices(Graph *pGraph, size_t *pSize);
+
+/**
+* Add edge to graph
+* pGraph    pointer to graph
+* u, v      graph edges
+* Possible errors:
+* GRAPH_RETURN_INVALID_PARAMETER
+*   - "pGraph" is NULL
+* GRAPH_RETURN_SAME_VERTEX
+*   - "u" is equal to "v"
+* GRAPH_RETURN_OUT_OF_BOUNDS
+*   - "u" or "v" is larger than the number of vertices in the graph
+* GRAPH_RETURN_CONTAINS_EDGE
+*   - Edge "uv" already exists in the graph
+* GRAPH_RETURN_UNKNOWN_ERROR
+* GRAPH_RETURN_MEMORY
+* GRAPH_RETURN_FATAL_ERROR
+*/
 GraphReturnID graphAddEdge(Graph *pGraph, size_t u, size_t v);
+
+/**
+* Remove edge from graph
+* pGraph    pointer to graph
+* u, v      graph edges
+* Possible errors:
+* GRAPH_RETURN_INVALID_PARAMETER
+*   - "pGraph" is NULL
+* GRAPH_RETURN_SAME_VERTEX
+*   - "u" is equal to "v"
+* GRAPH_RETURN_OUT_OF_BOUNDS
+*   - "u" or "v" is larger than the number of vertices in the graph
+* GRAPH_RETURN_DOES_NOT_CONTAIN_EDGE
+*   - Edge "uv" does not exist in the graph
+* GRAPH_RETURN_UNKNOWN_ERROR
+* GRAPH_RETURN_FATAL_ERROR
+*/
 GraphReturnID graphRemoveEdge(Graph *pGraph, size_t u, size_t v);
+
+/**
+* Check whether graph contains edge or not
+* pGraph    pointer to graph
+* u, v      graph edges
+* Possible errors:
+* GRAPH_RETURN_INVALID_PARAMETER
+*   - "pGraph" is NULL
+* GRAPH_RETURN_CONTAINS_EDGE
+*   - Edge "uv" exists in the graph
+* GRAPH_RETURN_DOES_NOT_CONTAIN_EDGE
+*   - Edge "uv" does not exist in the graph
+* GRAPH_RETURN_OUT_OF_BOUNDS
+*   - "u" or "v" is larger than the number of vertices in the graph
+* GRAPH_RETURN_UNKNOWN_ERROR
+*/
 GraphReturnID graphContainsEdge(Graph *pGraph, size_t u, size_t v);
+
+/**
+* Free graph structure from memory
+* pGraph    pointer to graph
+*/
 void graphDestroy(Graph *pGraph);
 
 #endif
--- graph/include/set.h
+++ graph/include/set.h
@@ -3,8 +3,8 @@
 
 /**
 * A Set starts empty.
-* You are able to add and remove numbers.
-* A number can go from 0 to 2^32 - 1.
+* You are able to add and remove 64-bit values
+* and check if values are contained within a set.
 */
 
 typedef enum
@@ -28,10 +28,59 @@ SetReturnID;
 
 typedef struct Set Set;
 
+/**
+* Create an empty set
+* ppSet     address of pointer to set
+* Possible errors:
+* SET_RETURN_INVALID_PARAMETER
+*   - "ppSet" is NULL
+* SET_RETURN_MEMORY
+*/
 SetReturnID setCreate(Set **ppSet);
+
+/**
+* Check whether set contains value or not
+* pSet      pointer to set
+* value     value to be checked
+* Possible errors:
+* SET_RETURN_INVALID_PARAMETER
+*   - "pSet" is NULL
+* SET_RETURN_CONTAINS
+*   - set contains value
+* SET_RETURN_DOES_NOT_CONTAIN
+*   - set does not contain value
+*/
 SetReturnID setContains(Set *pSet, size_t value);
+
+/**
+* Adds value to set
+* pSet      pointer to set
+* value     value to be added
+* Possible errors:
+* SET_RETURN_INVALID_PARAMETER
+*   - "pSet" is NULL
+* SET_RETURN_CONTAINS
+*   - set already contains value
+* SET_RETURN_MEMORY
+*/
 SetReturnID setAdd(Set *pSet, size_t value);
+
+/**
+* Remove value from set
+* pSet      pointer to set
+* value     value to be removed
+* Possible errors:
+* SET_RETURN_INVALID_PARAMETER
+*   - "pSet" is NULL
+* SET_RETURN_DOES_NOT_CONTAIN
+*   - set does not contain value
+*/
 SetReturnID setRemove(Set *pSet, size_t value);
+
+/**
+* Free set structure from memory
+* pSet      pointer to set
+*/
 void setDestroy(Set *pSet);
 
 #endif
