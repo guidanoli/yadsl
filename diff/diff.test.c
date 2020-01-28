8e19fe5612b8b8f16cb2a78afa750b1b62c0be39
Guilherme Dantas <guidanoli@hotmail.com>
2020-01-28 00:23:11 -0300

    Added test cases for difftest + travis.yml

--- diff/diff.test.c
+++ diff/diff.test.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include "diff.h"
 
 int main(int argc, char **argv)
 {
     size_t cost;
-    if (argc != 3) return 1;
+    if (argc < 3) return 1;
     cost = diff(argv[1], argv[2]);
-    if (cost == DIFFERR) return 1;
+    if (cost == DIFFERR) return 0;
+    if (argc > 3) {
+        // Automated testing
+        if (strcmp(argv[3], "EQUAL") == 0) {
+            return !(cost == 0);
+        } else if (strcmp(argv[3], "DIFFERENT") == 0) {
+            return !(cost != 0);
+        } else {
+            fprintf(stderr, "Unknown parameter '%s'\n", argv[3]);
+            return 1;
+        }
+    }
     printf("difference = %d\n", cost);
     return 0;
 }
