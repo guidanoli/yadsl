0806d97379dc2dca0c251c05bd06652cc7fe38f0
Guilherme Dantas <guidanoli@hotmail.com>
2020-02-02 16:28:38 -0300

    settest and difftest
    * Added set test module and script
    * Modified diff test script

--- set/set.test.c
+++ set/set.test.c
@@ -2,217 +2,198 @@
 #include <string.h>
 #include "set.h"
 #include "var.h"
+#include "tester.h"
 
 /* Help */
 
-static const char *helpStrings[] = {
+const char *TesterHelpStrings[] = {
     "This is an interactive module of the set library",
     "You interact with a single set object at all times",
-    "Actions to the set are parsed by the command line arguments",
     "",
     "The registered actions are the following:",
-    "s\tprints current set size",
-    "c\tprints current number pointed by the cursor",
-    "f\tmake cursor point to smallest number in set",
-    "l\tmake cursor point to biggest number in set",
-    "p\tmake cursor point to number directly smaller (previous)",
-    "n\tmake cursor point to number directly bigger (next)",
-    "X+\tadd number X to set",
-    "X-\tremove number X from set",
-    "X?\tcheck if number X is contained in the set",
-    "-<cf>\tcheck if there is a number that satisfies function",
-    "",
-    "The registered custom functions are the following:",
-    /* Follows custom functions here... */
+    "/save <var>               save variable",
+    "/contains [YES/NO]        check if set contains saved variable",
+    "/filter <var> [YES/NO]    check if filter matches one variable",
+    "/filtersave <var>         filter variable and save it",
+    "/add                      add saved variable to set",
+    "/remove                   remove saved variable from set",
+    "/current <expected>       get variable pointed by the cursor",
+    "/size <expected>          get set size",
+    "/previous                 move cursor to previous variable",
+    "/next                     move cursor to next variable",
+    "/first                    move cursor to first variable",
+    "/last                     move cursor to last variable",
     NULL, /* Sentinel */
 };
 
-/* Custom functions */
+TesterReturnValue convertReturn(SetReturnID setId) {
+    switch (setId) {
+    case SET_RETURN_OK:
+        return TESTER_RETURN_OK;
+    case SET_RETURN_INVALID_PARAMETER:
+        return TesterExternalReturnValue("invalid");
+    case SET_RETURN_MEMORY:
+        return TesterExternalReturnValue("malloc");
+    case SET_RETURN_CONTAINS:
+        return TesterExternalReturnValue("contains");
+    case SET_RETURN_DOES_NOT_CONTAIN:
+        return TesterExternalReturnValue("containsnot");
+    case SET_RETURN_EMPTY:
+        return TesterExternalReturnValue("empty");
+    case SET_RETURN_OUT_OF_BOUNDS:
+        return TesterExternalReturnValue("bounds");
+    default:
+        return TesterExternalReturnValue("unknown");
+    }
+}
 
-// Macros
-#define CF_MAX_NAME_SIZE 64
-#define CF_NAME(label) func_ ## label
-#define CF_PROTOTYPE(label) static int CF_NAME(label) (void *item, void *arg)
-#define CF(label, desc) {CF_NAME(label), #label, desc}
+/* Set object */
+Set *pSet = NULL;
+Variable *pVariable = NULL;
 
-// Function prototypes
-CF_PROTOTYPE(even);
-CF_PROTOTYPE(square);
+char buffer[TESTER_BUFFER_SIZE], arg[TESTER_BUFFER_SIZE];
 
-struct customFunction {
-    int (*func)(void *item, void *arg);
-    const char *label;
-    const char *description;
-}; 
-
-static struct customFunction customFunctions[] = {
-    CF(even, "even number"),
-    CF(square, "perfect square"),
-    { NULL, NULL, NULL }, /* Sentinel */
-};
-
-/* Private functions prototypes */
-
-static char *parseCmds(Set **ppSet, SetReturnID *pSetId, unsigned int *pActionIndex, char **actions, int actionCount);
-static void printHelpMessage();
-
-/* Main function */
-
-int main(int argc, char **argv)
+TesterReturnValue TesterInitCallback()
 {
-    Set *testSet = NULL;
-    SetReturnID setId = SET_RETURN_OK;
-    unsigned int actionIndex = 0;
-    char *errorString = NULL;
-    if (argc == 1) {
-        printHelpMessage();
+    SetReturnID setId;
+    if (setId = setCreate(&pSet))
+        return convertReturn(setId);
+    return TESTER_RETURN_OK;
+}
+
+#define matches(a, b) (strcmp(a, b) == 0)
+
+int filterItem(void *item, void *arg)
+{
+    Variable *varItem = (Variable *) item;
+    Variable *varArg = (Variable *) arg;
+    int result;
+    if (varCompare(varItem, varArg, &result))
         return 0;
-    }
-    /* ignore program name */
-    errorString = parseCmds(&testSet, &setId, &actionIndex, argv + 1, argc - 1);
-    if (testSet != NULL)
-        setDestroy(testSet);
-    testSet = NULL;
-    /* print error message, if there is */
-    if (errorString != NULL) {
-        if (setId != SET_RETURN_OK) {
-            fprintf(stderr, "Error #%d on action #%d (%s): %s\n",
-                setId, actionIndex, argv[actionIndex], errorString);
-        } else {
-            fprintf(stderr, "Error on action #%d (%s): %s\n",
-                actionIndex, argv[actionIndex], errorString);
-        }
-        return 1;
-    } else {
-        fprintf(stdout, "No errors.\n");
-    }
-    return 0;
+    return result;
 }
 
-static char *parseCmds(Set **ppSet, SetReturnID *pSetId, unsigned int *pActionIndex, char **actions, int actionCount)
+TesterReturnValue TesterParseCallback(const char *command)
 {
-    unsigned long number;
-    char c;
-    int i;
-    char buffer[CF_MAX_NAME_SIZE];
-    if (*pSetId = setCreate(ppSet)) {
-        if (*pSetId == SET_RETURN_MEMORY)
-            *ppSet = NULL;
-        return "Could not create set";
-    }
-    for (i = 0; i < actionCount; ++i) {
-        *pActionIndex = i+1;
-        if (sscanf(actions[i], "%lu%c", &number, &c) == 2) {
-            switch (c) {
-            case '-':
-                if (*pSetId = setRemove(*ppSet, number))
-                    return "Could not remove number from set";
-                break;
-            case '+':
-                if (*pSetId = setAdd(*ppSet, number))
-                    return "Could not add number to set";
-                break;
-            case '?':
-                *pSetId = setContains(*ppSet, number);
-                switch (*pSetId) {
-                    case SET_RETURN_CONTAINS:
-                        fprintf(stdout, "[%u] Contains %lu\n", *pActionIndex, number);
-                        break;
-                    case SET_RETURN_DOES_NOT_CONTAIN:
-                        fprintf(stdout, "[%u] Does not contain %lu\n", *pActionIndex, number);
-                        break;
-                    default:
-                        return "Cannot not assert if set contains number or not";
-                }
-                break;
-            default:
-                return "Unknown command";
+    SetReturnID setId = SET_RETURN_OK;
+    Variable *temp;
+    if matches(command, "save") {
+        if (TesterParseArguments("s", buffer) != 1)
+            return TESTER_RETURN_ARGUMENT;
+        if (varCreate(buffer, &temp))
+            return TESTER_RETURN_MALLOC;
+        if (setContainsItem(pSet, pVariable) != SET_RETURN_CONTAINS)
+            varDestroy(pVariable);
+        pVariable = temp;
+    } else if matches(command, "contains") {
+        int expected, actual;
+        if (TesterParseArguments("s", arg) != 1)
+            return TESTER_RETURN_ARGUMENT;
+        expected = matches(arg, "YES");
+        if (pVariable == NULL)
+            TesterLog("Found no variable saved. Checking if contains NULL.");
+        setId = setContainsItem(pSet, pVariable);
+        actual = (setId == SET_RETURN_CONTAINS);
+        if (actual != expected)
+            return TESTER_RETURN_RETURN;
+        else
+            setId = SET_RETURN_OK;
+    } else if matches(command, "filter") {
+        int actual, expected;
+        Variable *foundVar;
+        if (TesterParseArguments("ss", buffer, arg) != 2)
+            return TESTER_RETURN_ARGUMENT;
+        if (varCreate(buffer, &temp))
+            return TESTER_RETURN_MALLOC;
+        expected = matches(arg, "YES");
+        setId = setFilterItem(pSet, filterItem, temp, &foundVar);
+        varDestroy(temp);
+        actual = (setId == SET_RETURN_OK);
+        if (actual != expected)
+            return TESTER_RETURN_RETURN;
+        else
+            setId = SET_RETURN_OK;
+    } else if matches(command, "filtersave") {
+        Variable *foundVar;
+        if (TesterParseArguments("s", buffer) != 1)
+            return TESTER_RETURN_ARGUMENT;
+        if (varCreate(buffer, &temp))
+            return TESTER_RETURN_MALLOC;
+        setId = setFilterItem(pSet, filterItem, temp, &foundVar);
+        if (setId == SET_RETURN_OK) {
+            if (pVariable != NULL &&
+                setContainsItem(pSet, pVariable) != SET_RETURN_CONTAINS)
+                varDestroy(pVariable);
+            pVariable = foundVar;
+        }
+        varDestroy(temp);
+    } else if matches(command, "add") {
+        if (pVariable == NULL)
+            TesterLog("Found no variable saved. Adding NULL.");
+        setId = setAddItem(pSet, pVariable);
+    } else if matches(command, "remove") {
+        if (pVariable == NULL)
+            TesterLog("Found no variable saved. Removing NULL.");
+        setId = setRemoveItem(pSet, pVariable);
+    } else if matches(command, "current") {
+        Variable *curr;
+        if (TesterParseArguments("s", buffer) != 1)
+            return TESTER_RETURN_ARGUMENT;
+        if (varCreate(buffer, &temp))
+            return TESTER_RETURN_MALLOC;
+        if (setId = setGetCurrentItem(pSet, &curr)) {
+            varDestroy(temp);
+        } else {
+            int equal;
+            if (curr == NULL) {
+                varDestroy(temp);
+                TesterLog("The current item is NULL");
+                return TESTER_RETURN_RETURN;
             }
-        } else if (sscanf(actions[i], "%c", &c) == 1) {
-            struct customFunction *cfunc = customFunctions;
-            int matchedCustomFunction = 0;
-            switch (c) {
-            case '-':
-                if (strlen(actions[i]) >= CF_MAX_NAME_SIZE - 1)
-                    return "Custom function exceeds the maximum size";
-                if (sscanf(actions[i], "-%s", buffer) != 1)
-                    return "Could not parse custom function name";
-                for (; cfunc->func && !matchedCustomFunction; ++cfunc) {
-                    if (!strcmp(buffer, cfunc->label)) {
-                        matchedCustomFunction = 1;
-                        *pSetId = setFilterItem(*ppSet, cfunc->func, NULL, (void **) &number);
-                        switch (*pSetId) {
-                        case SET_RETURN_OK:
-                            fprintf(stdout, "[%u] Contains %lu (%s)\n", *pActionIndex, number, cfunc->description);
-                            break;
-                        case SET_RETURN_DOES_NOT_CONTAIN:
-                            fprintf(stdout, "[%u] Does not contain %s\n", *pActionIndex, cfunc->description);
-                            break;
-                        default:
-                            return "Cannot assert if set contains number or not";
-                       }
-                    }
-                }
-                if (!matchedCustomFunction)
-                    return "Custom function not found";
-                break;
-            case 'p':
-                if (*pSetId = setPreviousValue(*ppSet))
-                    return "Could not go to the previous value";
-                break;
-            case 'n':
-                if (*pSetId = setNextValue(*ppSet))
-                    return "Could not go to the next value";
-                break;
-            case 'c':
-                if (*pSetId = setGetCurrent(*ppSet, &number))
-                    return "Could not get current value";
-                fprintf(stdout, "[%u] %lu\n", *pActionIndex, number);
-                break;
-            case 'f':
-                if (*pSetId = setFirstValue(*ppSet))
-                    return "Could not go to the first value";
-                break;
-            case 'l':
-                if (*pSetId = setLastValue(*ppSet))
-                    return "Could not go to the last value";
-                break;
-            case 's':
-                if (*pSetId = setGetSize(*ppSet, &number))
-                    return "Could not get set size";
-                fprintf(stdout, "[%u] %lu\n", *pActionIndex, number);
-                break;
-            default:
-                return "Unknown command";
+            varCompare(temp, curr, &equal);
+            varDestroy(temp);
+            if (!equal) {
+                varWrite(curr, stdout);
+                TesterLog(" is the current item");
+                return TESTER_RETURN_RETURN;
             }
         }
+    } else if matches(command, "size") {
+        unsigned long expected, actual;
+        if (TesterParseArguments("i", &expected) != 1)
+            return TESTER_RETURN_ARGUMENT;
+        setId = setGetSize(pSet, &actual);
+        if (setId == SET_RETURN_OK && actual != expected)
+            return TESTER_RETURN_RETURN;
+    } else if matches(command, "previous") {
+        setId = setPreviousItem(pSet);
+    } else if matches(command, "next") {
+        setId = setNextItem(pSet);
+    } else if matches(command, "first") {
+        setId = setFirstItem(pSet);
+    } else if matches(command, "last") {
+        setId = setLastItem(pSet);
+    } else {
+        return TESTER_RETURN_COMMAND;
     }
-    return NULL;
+    return convertReturn(setId);
 }
 
-static void printHelpMessage() {
-    const char **str = helpStrings;
-    struct customFunction *cfunc = customFunctions;
-    for (; *str; ++str) puts(*str);
-    for (; cfunc->func; ++cfunc)
-        printf("%s\tmatches %s\n", cfunc->label, cfunc->description);
+void freeItem(void *item, void *arg) {
+    Variable *var = (Variable *) item;
+    if (var == pVariable)
+        pVariable = NULL;
+    varDestroy(var);
 }
 
-CF_PROTOTYPE(even) {
-    unsigned long number = (unsigned long) item;
-    return number % 2 == 0;
-}
-
-CF_PROTOTYPE(square) {
-    unsigned long number = (unsigned long) item;
-    unsigned long aux = 1;
-    // makes use of the following equation:
-    // 1 + 3 + 5 + ... + (2*n - 1) = n^2
-    while (number) {
-        if (aux > number)
-            return 0;
-        number -= aux;
-        aux += 2;
-    }
-    return 1;
+TesterReturnValue TesterExitCallback()
+{
+    setDestroyDeep(pSet, freeItem, NULL);
+    if (pVariable != NULL)
+        varDestroy(pVariable);
+#ifdef _DEBUG
+    if (varGetRefCount())
+        return TESTER_RETURN_MEMLEAK;
+#endif
+    return TESTER_RETURN_OK;
 }
