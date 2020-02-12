e88643324555010d917fbe482b0cb88d8a5853df
Guilherme Dantas <guidanoli@hotmail.com>
2020-02-12 02:40:33 -0300

    More pydiff test cases

--- diff/python/diff_test.py
+++ diff/python/diff_test.py
@@ -3,9 +3,23 @@ with open(".paths", "r") as f:
 	for line in f:
 		sys.path.append(line.replace('\n', ''))
 ###############################################################################
-print(sys.path)
-
 import pydiff
 
 def test_empty():
 	assert pydiff.diff("","") == 0
+
+def test_equal():
+	assert pydiff.diff(" "," ") == 0
+	assert pydiff.diff("a","a") == 0
+	assert pydiff.diff("abc123","abc123") == 0
+	assert pydiff.diff("good ol' string","good ol' string") == 0
+
+def test_different():
+	assert pydiff.diff("a", "") != 0
+	assert pydiff.diff("a", " ") != 0
+	assert pydiff.diff("", " ") != 0
+	assert pydiff.diff("abc", "abd") != 0
+	assert pydiff.diff("space", "s p a c e") != 0
+	assert pydiff.diff("lspace", " lspace") != 0
+	assert pydiff.diff("rspace", "rspace ") != 0
+	assert pydiff.diff("case", "CaSe") != 0
