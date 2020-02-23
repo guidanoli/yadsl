4edc8e59a4aaed5d37cb04c37673bd90222eaa2a
Guilherme Dantas <guidanoli@hotmail.com>
2020-02-22 23:05:14 -0300

    Added guard for Py_UNREACHABLE not being defined (Python < v3.7)

--- graph/python/graph.py.c
+++ graph/python/graph.py.c
@@ -4,6 +4,12 @@
 #include <Python.h>
 #include <pymacro.h>
 
+#ifndef Py_UNREACHABLE
+#pragma message("Py_UNREACHABLE was not found, maybe you're using a Python " \
+"of version previous to v3.7. It will be then defined as a macro for abort().")
+#define Py_UNREACHABLE() abort()
+#endif
+
 #include "graph.h"
 
 typedef struct {
