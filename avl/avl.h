9153073f055544059ab135a9ea353ce7576d977c
Guilherme Dantas <guidanoli@hotmail.com>
2020-03-02 08:58:19 -0300

    Update avl.h

--- avl/avl.h
+++ avl/avl.h
@@ -8,14 +8,13 @@
 //   / ___ | |/ / /___
 //  /_/  |_|___/_____/
 //                    
-// An AVL tree is a self-balancing binary tree, that
-// guarantees that the tree does not degenerate into a
-// single-linked list, and assuring that its height is
-// less or equal to 1.44*log2(n+1.065)-0.328.
+// 	An AVL tree is a self-balancing binary tree
+// that guarantees that its height is always in the
+// magnitude of O(log(n)), being n the number of nodes.
 //
-// It is a better option for storing ordered and unique
-// data than linked lists in matter of time complexity
-// for most actions.
+// 	It is a better option for storing ordered and
+// unique data than linked lists in the matter of time
+// complexity for the following actions:
 //
 // +-----------+-----------+------------+
 // | Algorithm |  Average  | Worst Case |
@@ -26,6 +25,22 @@
 // | Delete    | O(log(n)) | O(log(n))  |
 // +-----------+-----------+------------+
 //
+// HINTS
+// -----
+//
+// 	- You may use AVLReturnId to check for errors
+// by using it as a boolean value, since AVL_RETURN_OK
+// will always assume the value 0. This does not exclude
+// the importance to check other values returned by ref.
+//
+// DOCUMENTATION
+// -------------
+//
+// 	- (optional) arguments can be NULL.
+// 	- (return) arguments should be references
+//      - [!] are warning markers for client use
+//      - Possible errors exclude AVL_RETURN_OK
+// 
 
 typedef enum
 {
@@ -45,8 +60,10 @@ typedef struct AVL AVL;
 /**
 * Create an empty tree
 * ppTree   (return) pointer to tree
-* cmpObjs  function that compares two objects
-* freeObj  function that deallocates object
+* cmpObjs  (optional) function that compares two objects
+*          	- if NULL, does shallow address comparison
+* freeObj  (optional) function that deallocates object
+*          	- if NULL, does not take ownership over objects
 * arg      argument passed to cmpObjs
 * Possible errors:
 * AVL_RETURN_INVALID_PARAMETER
@@ -61,7 +78,9 @@ AVLReturnId avlCreate(AVL **ppTree,
 * Insert object in tree
 * pTree    pointer to tree
 * object   object to be inserted
-* pExists  (return) object was in tree before
+* pExists  (optional return) object was in tree before
+*          	- if NULL, won't be able to check whether
+*          	object was truly inserted in tree or not.
 * Possible errors:
 * AVL_RETURN_INVALID_PARAMETER
 *	- "pTree" is NULL
@@ -75,7 +94,9 @@ AVLReturnId avlInsert(AVL *pTree, void *object, int *pExists);
 * Search for object in tree
 * pTree    pointer to tree
 * object   object to be searched for
-* pExists  (return) object is in tree
+* pExists  (optional return) whether object is in tree
+*          	- if NULL, won't be able to check whether
+*          	object is in tree or not.
 * Possible errors:
 * AVL_RETURN_INVALID_PARAMETER
 *	- "pTree" is NULL
@@ -86,11 +107,12 @@ AVLReturnId avlSearch(AVL *pTree, void *object, int *pExists);
 /**
 * Traverse through tree
 * pTree      pointer to tree
-* visit_cb   function called for every visted object
-*            if it returns a value other than NULL (0), traversing stops
-*            and this value is returned by reference on *pReturn
+* visit_cb   (optional) function called for every visted object
+*            	- if NULL, it will simply not be called.
+*            	- if it returns a value other than NULL (0), traversing
+*            	stops and this value is returned by reference on *pReturn.
 * arg        argument passed to visit_cb
-* pReturn    (return) last return value by visit_cb
+* pReturn    (optional return) last return value by visit_cb
 * Possible errors:
 * AVL_RETURN_INVALID_PARAMETER
 *	- "pTree" is NULL
@@ -103,7 +125,9 @@ AVLReturnId avlTraverse(AVL *pTree,
 * Delete object from tree
 * pTree    pointer to tree
 * object   object to be deleted
-* pExists  (return) object was in tree before
+* pExists  (optional return) object was in tree before
+*          	- if NULL, won't be able to check whether
+*          	it was in tree before or not.
 * Possible errors:
 * AVL_RETURN_INVALID_PARAMETER
 *	- "pTree" is NULL
