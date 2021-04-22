d5dacf21240bc77f5ad2f3c4928661b106773efa
guidanoli <guidanoli@hotmail.com>
2021-04-22 13:15:04 -0300

    Make lt an executable

--- src/lt/lt.c
+++ src/lt/lt.c
@@ -1,63 +1,129 @@
 #include <stdio.h>
-#include <stdarg.h>
 
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 
-#include "lt.h"
-
-static const char* const LT_TRACEBACK = "LT_TRACEBACK";
-
-static const char* script_name;
-static const char* program_name;
-static lua_State* L;
-
-static void print(const char* fmt, ...)
+/**
+ * Checks if the value at the given valid index is of a
+ * given expected type. If not, a Lua error is raised
+ * informing the value of a given name has the wrong type.
+ */
+static void lt_checktype(lua_State* L, int idx, const char* name, int expected_type)
 {
-	va_list va;
-	va_start(va, fmt);
-	fprintf(stderr, "%s: ", program_name);
-	vfprintf(stderr, fmt, va);
-	fprintf(stderr, "\n");
-	va_end(va);
-}
-
-static void printluaerr(int st)
-{
-	switch (st)
+	if (lua_type(L, idx) != expected_type)
 	{
-		case LUA_OK:
-			break;
-		case LUA_ERRRUN:
-			print("Runtime error");
-			break;
-		case LUA_ERRMEM:
-			print("Memory allocation error");
-			break;
-		case LUA_ERRERR:
-			print("Error while running the message handler");
-			break;
-		case LUA_ERRSYNTAX:
-			print("Syntax error");
-			break;
-		case LUA_YIELD:
-			print("Thread yielded");
-			break;
-		case LUA_ERRFILE:
-			print("File-related error");
-			break;
-		default:
-			print("Unknown error");
+		luaL_error(L, "%s must be %s, not %s", name,
+											   lua_typename(L, expected_type),
+		                                       luaL_typename(L, idx));
 	}
 }
 
 /**
- * Initializes the program with the arguments provided on startup
- * Returns the current program status
+ * Run the main Lua program
+ * Arguments:
+ * [1] = script file name (string or nil)
  */
-static int initialize(int argc, char** argv)
+static int lt_main(lua_State* L)
 {
+	/* Lua stack indices */
+	const int script = 1;
+	const int traceback = 2;
+	const int namestbl = 3;
+	const int errorstbl = 4;
+	const int testbenchtbl = 5;
+
+	const char* scriptname;
+	lua_Integer i, n = 0;
+
+	lua_getglobal(L, "debug");         /* script debug */
+	lt_checktype(L, -1, "debug", LUA_TTABLE);
+
+	lua_getfield(L, -1, "traceback");  /* script debug traceback */
+	lt_checktype(L, -1, "debug.traceback", LUA_TFUNCTION);
+
+	lua_remove(L, -2);                 /* script traceback */
+	lua_newtable(L);                   /* script traceback namestbl */
+	lua_newtable(L);                   /* script traceback namestbl errorstbl */
+
+	if (lua_isstring(L, script))
+	{
+		scriptname = lua_tostring(L, script);
+	}
+	else
+	{
+		/* Note: if a script file is not specified,
+		 * then luaL_loadfile reads from stdin */
+		scriptname = NULL;
+	}
+
+	if (luaL_loadfile(L, scriptname) ||  /* chunk */
+	    lua_pcall(L, 0, 1, traceback))   /* testbenchtbl */
+	{
+		return lua_error(L);             /* errmsg */
+	}
+
+	lt_checktype(L, -1, "test bench", LUA_TTABLE);
+
+	lua_pushnil(L);                         /* nil */
+
+	while (lua_next(L, testbenchtbl))       /* key value */
+	{
+		const char* key;
+
+		lt_checktype(L, -2, "test case name", LUA_TSTRING);
+
+		key = lua_tostring(L, -2);
+
+		lt_checktype(L, -1, "test case", LUA_TFUNCTION);
+		
+		lua_pushvalue(L, testbenchtbl);     /* key value testbenchtbl */
+
+		fprintf(stderr, "%s ... ", key);
+
+		if (lua_pcall(L, 1, 0, traceback))  /* key */
+		{
+			n++;                            /* key err */
+			lua_rawseti(L, errorstbl, n);   /* key */
+			lua_pushvalue(L, -1);           /* key key */
+			lua_rawseti(L, namestbl, n);    /* key */
+			fputs("FAIL\n", stderr);
+		}
+		else
+		{
+			fputs("ok\n", stderr);
+		}
+	}
+
+	for (i = 1; i <= n; ++i)
+	{
+		lua_rawgeti(L, namestbl, i);      /* name */
+		lua_rawgeti(L, errorstbl, i);     /* name err */
+		fprintf(stderr, "\nFAIL: %s\n%s\n", lua_tostring(L, -2),
+		                                    lua_tostring(L, -1));
+		lua_pop(L, 2);                    /* */
+	}
+
+	if (n == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		fputs("\n", stderr);
+		return luaL_error(L, "%I test cases failed", n);
+	}
+}
+
+/**
+ * Main program flow
+ */
+int main(int argc, char** argv)
+{
+	const char* program;
+	lua_State* L;
+	int status;
+
 	/* Specified in 5.1.2.2.1 (Program Startup) of ISO/IEC 9899:TC3
 	 *
 	 * If the value of argc is greater than zero, the string pointed to by argv[0]
@@ -66,159 +132,42 @@ static int initialize(int argc, char** argv)
 	 */
 	if (argc > 0 && argv[0][0] != '\0')
 	{
-		program_name = argv[0];
+		program = argv[0];
 	}
 	else
 	{
-		program_name = "lt";
-	}
-
-	if (argc >= 2)
-	{
-		script_name = argv[1];
+		program = "lt";
 	}
 
 	L = luaL_newstate();
 
 	if (L == NULL)
 	{
-		print("Could not allocate Lua state");
+		fprintf(stderr, "%s: Could not allocate Lua state\n", program);
 		return LUA_ERRMEM;
 	}
 
-	/* Load Lua libraries */
 	luaL_openlibs(L);
-
-	if (!lua_checkstack(L, 7))
-	{
-		print("Could not pre-allocate stack space");
-		return LUA_ERRMEM;
-	}
-
-	/* Load user library */
-	if (lt_openlib(L) < 1)
-	{
-		print("Could not open user library");
-		return LUA_ERRRUN;
-	}
-
-	lua_settop(L, 1);                   /* lib */
-
-	return 0;
-}
-
-/**
- * Run the program
- * Receives the current program status
- * Returns the (new) program status
- */
-static int run(int st)
-{
-	if (st)
-	{
-		return st;
-	}
-
-	lua_getglobal(L, "debug");          /* lib debug */
-	lua_getfield(L, -1, "traceback");   /* lib debug traceback */
-	lua_remove(L, -2);                  /* lib traceback */
-
-	/* Note: if script is not specified, script_name = NULL,
-	 * and luaL_loadfile reads from stdin */
-	st = luaL_loadfile(L, script_name); /* lib traceback func */
 	
-	if (st)
+	lua_pushcfunction(L, lt_main);
+
+	if (argc >= 2)
 	{
-		printluaerr(st);
-		return st;
+		lua_pushstring(L, argv[1]);
+	}
+	else
+	{
+		lua_pushnil(L);
 	}
 
-	st = lua_pcall(L, 0, 1, -2);        /* lib traceback ret */
-
-	if (st)
+	status = lua_pcall(L, 1, 0, 0);
+	
+	if (status)
 	{
-		if (st == LUA_ERRRUN)
-		{
-			fprintf(stderr, "%s\n", lua_tostring(L, -1));
-		}
-		printluaerr(st);
-		return st;
+		fprintf(stderr, "%s: %s\n", program, lua_tostring(L, -1));
 	}
 
-	if (!lua_istable(L, -1))
-	{
-		print("Expected script to return table");
-		return LUA_ERRRUN;
-	}
+	lua_close(L);
 
-	lua_pushnil(L);                       /* lib traceback tbl nil */
-
-	while (lua_next(L, -1))               /* lib traceback tbl key value */
-	{
-		size_t keylen;
-		const char* key;
-
-		if (lua_isstring(L, -2))
-		{
-			key = lua_tolstring(L, -2, &keylen);
-		}
-		else
-		{
-			print("Table key is %s, not string", lua_typename(L, lua_type(L, -2)));
-			lua_pop(L, 1);                /* lib traceback tbl key */
-			continue;
-		}
-
-		if (lua_isfunction(L, -1))
-		{
-			lua_pushvalue(L, -3);         /* lib traceback tbl key value tbl */
-			lua_pushvalue(L, -6);         /* lib traceback tbl key value tbl lib */
-			st = lua_pcall(L, 2, 0, -6);  /* lib traceback tbl key */
-			
-			if (st)
-			{
-				if (st == LUA_ERRRUN)
-				{
-					fprintf(stderr, "%s\n", lua_tostring(L, -1));
-				}
-				printluaerr(st);
-				return st;
-			}
-		}
-		else
-		{
-			print("Table value is %s, not function", lua_typename(L, lua_type(L, -1)));
-			lua_pop(L, 1);                /* lib traceback tbl key */
-			continue;
-		}
-	}
-
-	return 0;
-}
-
-/**
- * Terminates the program and all its dependencies
- * Receives the current program status
- * Returns the (new) program status
- */
-static int terminate(int st)
-{
-	if (L != NULL)
-	{
-		lua_close(L);
-		L = NULL;
-	}
-
-	return st;
-}
-
-/**
- * Main program flow
- */
-int main(int argc, char** argv)
-{
-	int st;
-	st = initialize(argc, argv);
-	st = run(st);
-	return terminate(st);
+	return status;
 }
--- src/lt/lt.h
+++
@@ -1,8 +0,0 @@
-#ifndef lt_h
-#define lt_h
-
-#include "lua.h"
-
-extern int lt_openlib(lua_State *L);
-
-#endif
